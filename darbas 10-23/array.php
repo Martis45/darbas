40fcf74f57eb9801e1720a6502df97255015e887
Martis45 <113464473+Martis45@users.noreply.github.com>
2023-10-23 11:33:28 +0300

    Add files via upload

---
+++ darbas 10-23/array.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    // Array
+    $array_1 = [120, 25, 56, 32];
+    $array_1 = [
+        0 => 123,
+        1 => 25,
+        2 => 30,
+        3 => 74
+    ];
+    // var_dump($array_1);
+    //echo $array_1[0];
+
+
+
+    $array_2 = [
+        'vardas' => 'Jonas',
+        'amzius' => '60',
+        'megstamiausi dalykai' => ['Matematika', 'Javascript']
+    ];
+// var_dump($array_2);
+//echo $array_2['vardas'] . ', jam yra' . $array_2['amzius'] . '';
+//echo'<br />';
+//echo 'Megstamiausi dalykai:' . implode(',', $array_2['megstamiausi_dalykai']);
+//echo '<br />';
+    ?>
+</body>
+</html>
